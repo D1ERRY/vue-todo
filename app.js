bd989171746c6bf8201e5e4189e24a74c615e55e
d1erry <d1erry@vk.com>
2022-11-09 03:38:52 +0500

    Update 0.2.2

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 const App = new Vue({
   el: "#app",
   data() {
@@ -6,17 +7,35 @@ const App = new Vue({
       title: "Список заметок",
       inputValue: "",
       checkBox: false,
-      notes: [],
+      userName: "",
+      notes: [
+        // {
+        //   id: 1,
+        //   title: "Сходить в кино",
+        // },
+        // {
+        //   id: 2,
+        //   title: "Покушать",
+        // },
+      ],
+      editText: "",
+      noteState: Vue.ref("all"),
     };
   },
   methods: {
     addNewNote() {
       if (this.inputValue.trim() != "") {
         if (this.inputValue.length > 50) {
-          this.notes.unshift(this.inputValue.substr(0, 50) + "...");
+          this.notes.unshift({
+            id: uuidv4(),
+            title: this.inputValue.substr(0, 50) + "...",
+          });
           this.inputValue = "";
         } else {
-          this.notes.unshift(this.inputValue);
+          this.notes.unshift({
+            id: uuidv4(),
+            title: this.inputValue,
+          });
           this.inputValue = "";
         }
       }
@@ -26,25 +45,26 @@ const App = new Vue({
     },
     changeText(idx) {
       console.log("Редактирование");
-
-      let arr = prompt(`Изменить ${this.notes[idx]}:`);
-      console.log(arr);
-      if (arr == null) {
+      console.log(this.notes[idx].id);
+      let arr = prompt(`Изменить ${this.notes[idx].title}:`);
+      if (arr == null || arr.trim() == "") {
         this.inputValue = "";
       } else {
         if (arr.trim() != "" || arr != null) {
+          let oldNote = this.notes[idx];
+          oldNote.title = arr;
           this.removeNote(idx);
-          this.notes.splice(idx, 0, arr);
+          this.notes.splice(idx, 0, oldNote);
         }
       }
     },
-    say(el) {
-      console.log(el);
+    say() {
+      console.log("Change");
     },
   },
   computed: {
     filerList() {
-      return this.notes.filter((i) => i.toLowerCase().includes(this.inputValue.toLowerCase()));
+      return this.notes.filter((i) => i.title.toLowerCase().includes(this.inputValue.toLowerCase()));
     },
   },
   mounted() {
@@ -52,20 +72,28 @@ const App = new Vue({
       this.inputValue = localStorage.inputValue;
     }
     if (localStorage.notes) {
-      this.notes = localStorage.notes.split(",");
+      this.notes = JSON.parse(localStorage.notes);
       // console.log("b");
+    }
+    if (localStorage.userName) {
+      this.userName = localStorage.userName;
     }
   },
   watch: {
     inputValue(value) {
       localStorage.inputValue = value;
     },
-    notes(value) {
-      localStorage.notes = value;
-      // console.log(localStorage.notes);
+    notes: {
+      handler(value) {
+        localStorage.notes = JSON.stringify(value);
+      },
+      deep: true,
+    },
+    userName(value) {
+      localStorage.userName = value;
     },
     checkBox(value) {
-      console.log(value);
+      // console.log(value);
     },
   },
 });
